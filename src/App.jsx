3c0b9a27c2e5dfbab671bd9bf0acae81487f1a31
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-02-22 10:40:14 -0600

    Add redux funtionality and styles

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPhotos } from "./redux/galleryState";
+function App() {
+  const dispatch = useDispatch();
+  const photos = useSelector((state) => state.gallery.photos);
+  console.log(photos);
+  useEffect(() => {
+    dispatch(getPhotos());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <h1>PHOTO GALLERY</h1>
+      <p>This is a photo gallery made using redux toolkit and redux thunk.</p>
+      <hr />
+      <div className="Gallery">
+        {photos.map((photo) => (
+          <img
+            key={photo.id}
+            src={photo.download_url}
+            alt={photo.author}
+            width="400"
+            height="400"
+          />
+        ))}
+      </div>
+      <button>View more</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+import galleryReducer from "./galleryState";
+export const store = configureStore({
+  reducer: { gallery: galleryReducer },
+});
